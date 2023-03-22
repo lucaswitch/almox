4295b9e8b00ed45c2bd6a056b8313fa6769060c0
Amanda <amanda.fo@sempreceub.com>
2023-03-22 16:01:20 -0300

    Pagina de login simples criada

--- front/pages/PaginaSignin.vue
+++ front/pages/PaginaSignin.vue
@@ -1,1 +1,43 @@
-<template>SignIn</template>
+<template>
+    <div class="container">
+      <form @submit.prevent="login">
+        <h2 class="mb-3">Login</h2>
+        <div class="input">
+          <label for="email">Email address</label>
+          <input
+            class="form-control"
+            type="text"
+            name="email"
+            placeholder="Email"
+          />
+        </div>
+        <div class="input">
+          <label for="password">Password</label>
+          <input
+            class="form-control"
+            type="password"
+            name="password"
+            placeholder="Senha"
+          />
+        </div>
+        <button type="submit" class="mt-4 btn-pers" id="login_button">
+          Login
+        </button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: "",
+        password: "",
+      };
+    },
+    methods: {
+      login() {
+      },
+    },
+  };
+  </script>
