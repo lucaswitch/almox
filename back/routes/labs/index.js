f5a9b8afa3e4f68088f1fe1401b07b96175ec8f9
Lucas Alves <lucas.developing@gmail.com>
2023-05-03 21:49:02 -0300

    Setup do banco de dados.

---
+++ back/routes/labs/index.js
@@ -0,0 +1,9 @@
+import { object, string } from "yup";
+import { validate } from "../../yup.js";
+import { Lab } from "../../models/index.js";
+/**
+ * Lida com request de login.
+ */
+export async function listLabs(request, response) {
+  return response.status(200).json({});
+}
