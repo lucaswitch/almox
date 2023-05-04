4b757256d9f2ea55387a785352b7626a8fbd1f24
Lucas Alves <lucas.developing@gmail.com>
2023-05-03 21:49:30 -0300

    Setup do banco de dados.

--- back/server.js
+++ back/server.js
@@ -1,17 +1,20 @@
-import express from 'express';
-import {signIn} from "./routes/sign-in/index.js";
-import bodyParser from 'body-parser';
+import express from "express";
+import { signIn } from "./routes/sign-in/index.js";
+import bodyParser from "body-parser";
+import { listLabs } from "./routes/labs/index.js";
+
 /**
  * Inicia o servidor.
  */
 export function startServer() {
-    let app = express()
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-    app.use(bodyParser.raw());
+  let app = express();
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.raw());
 
-    app.post('/sign-in', signIn);
-    app.listen(3000);
+  app.post("/sign-in", signIn);
+  app.get("/labs", listLabs);
+  app.listen(3000);
 
-    console.info('Server listening...');
+  console.info("Server listening...");
 }
