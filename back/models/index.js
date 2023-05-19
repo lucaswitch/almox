f8f9b32d8f766dc64f01bd9e3cc88e496fa1405e
Lucas Alves <lucas.developing@gmail.com>
2023-05-18 23:16:43 -0300

    Atualizaçõa.

---
+++ back/models/index.js
@@ -0,0 +1,27 @@
+"use strict";
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  username: process.env.MYSQL_DATABASE_USER,
+  password: process.env.MYSQL_DATABASE_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: "127.0.0.1",
+  dialect: "mysql",
+  use_env_variable: false,
+};
+
+export const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+    freezeTableName: true,
+  }
+);
+
+export default sequelize;
