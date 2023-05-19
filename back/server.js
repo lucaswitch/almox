85f742af88b46e95d4ddd2b55bbcb3a5e7281ff8
Lucas Alves <lucas.developing@gmail.com>
2023-05-18 22:44:08 -0300

    Atualizaçõa.

--- back/server.js
+++ back/server.js
@@ -1,26 +1,44 @@
 import express from "express";
-import { signIn } from "./routes/sign-in/index.js";
+import {signIn} from "./routes/sign-in/index.js";
 import bodyParser from "body-parser";
-import { listLabs } from "./routes/labs/index.js";
-import { listMaterials } from "./routes/materials/index.js";
-import { createAppointment } from "./routes/appointments/index.js";
+import {listLabs} from "./routes/labs/index.js";
+import {listMaterials} from "./routes/materials/index.js";
+import {createAppointment} from "./routes/appointments/index.js";
+import {Appointment} from "./models/appointment.js";
 
 /**
  * Inicia o servidor.
  */
 export function startServer() {
-  let app = express();
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  app.use(bodyParser.raw());
+    let app = express();
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(bodyParser.raw());
 
-  // Requisições
-  app.post("/sign-in", signIn);
-  app.get("/labs", listLabs);
-  app.get("/materials", listMaterials);
-  app.post("/appointments", createAppointment);
+    // Requisições
+    app.post("/sign-in", signIn);
+    app.get("/labs", listLabs);
+    app.get("/materials", listMaterials);
+    app.post("/appointments", createAppointment);
+    app.get(
+        "/appointments",
+        async function (request, response) {
+            const query = request.query;
+            let appointments = await Appointment.findAll();
+            if (query?.lab_id) {
+                appointments = appointments.filter(function (appointment) {
+                    return appointment.lab_id == query.lab_id
+                })
+            }
 
-  app.listen(3000);
+            return response.json(
+                appointments
+            )
+        }
+    );
 
-  console.info("Server listening...");
+
+    app.listen(80);
+
+    console.info("Server listening...");
 }
