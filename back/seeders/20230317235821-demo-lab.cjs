3f179aac5b75262e6671ef9e053fba1ddd930081
Lucas Alves <lucas.developing@gmail.com>
2023-05-18 23:17:58 -0300

    Atualizaçõa.

--- back/seeders/20230317235821-demo-lab.cjs
+++ back/seeders/20230317235821-demo-lab.cjs
@@ -1,28 +1,29 @@
 "use strict";
-const moment = require("moment");
-const { Lab } = require("../models/index.js");
+import moment from "moment";
+import {Lab} from "../models/index.js";
+
 /** @type {import("sequelize-cli").Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(Lab.tableName, [
-      {
-        name: "Laboratório A",
-        created_at: moment().utc().format("YYYY/MM/DD HH:mm:ss"),
-        updated_at: moment().utc().format("YYYY/MM/DD HH:mm:ss")
-      }, {
-        name: "Laboratório B",
-        created_at: moment().utc().format("YYYY/MM/DD HH:mm:ss"),
-        updated_at: moment().utc().format("YYYY/MM/DD HH:mm:ss")
-      },
-      {
-        name: "Laboratório C",
-        created_at: moment().utc().format("YYYY/MM/DD HH:mm:ss"),
-        updated_at: moment().utc().format("YYYY/MM/DD HH:mm:ss")
-      }
-    ], {});
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert(Lab.tableName, [
+            {
+                name: "Laboratório A",
+                created_at: moment().utc().format("YYYY/MM/DD HH:mm:ss"),
+                updated_at: moment().utc().format("YYYY/MM/DD HH:mm:ss")
+            }, {
+                name: "Laboratório B",
+                created_at: moment().utc().format("YYYY/MM/DD HH:mm:ss"),
+                updated_at: moment().utc().format("YYYY/MM/DD HH:mm:ss")
+            },
+            {
+                name: "Laboratório C",
+                created_at: moment().utc().format("YYYY/MM/DD HH:mm:ss"),
+                updated_at: moment().utc().format("YYYY/MM/DD HH:mm:ss")
+            }
+        ], {});
+    },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete(Lab.tableName, null, {});
-  }
+    async down(queryInterface, Sequelize) {
+        await queryInterface.bulkDelete(Lab.tableName, null, {});
+    }
 };
