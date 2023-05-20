4d15f6eb0e60038591775b477370ab9d918f2ccb
Lucas Alves <lucas.developing@gmail.com>
2023-05-20 11:01:41 -0300

    Atualizacoes.

--- back/server.js
+++ back/server.js
@@ -1,27 +1,29 @@
 import express from "express";
-import {signIn} from "./routes/sign-in/index.js";
+import cors from "cors";
+import { signIn } from "./routes/sign-in/index.js";
 import bodyParser from "body-parser";
-import {listLabs} from "./routes/labs/index.js";
-import {listMaterials} from "./routes/materials/index.js";
-import {createAppointment} from "./routes/appointments/index.js";
-import {Appointment} from "./models/appointment.js";
+import { listLabs } from "./routes/labs/index.js";
+import { listMaterials } from "./routes/materials/index.js";
+import { createAppointment } from "./routes/appointments/index.js";
+import { Appointment } from "./models/appointment.js";
 
 /**
  * Inicia o servidor.
  */
 export function startServer() {
-    let app = express();
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
-    app.use(bodyParser.raw());
+  let app = express();
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.raw());
 
-    // Requisições
-    app.post("/sign-in", signIn);
-    app.get("/labs", listLabs);
-    app.get("/materials", listMaterials);
-    app.post("/appointments", createAppointment);
+  // Requisições
+  app.post("/sign-in", signIn);
+  app.get("/labs", listLabs);
+  app.get("/materials", listMaterials);
+  app.post("/appointments", createAppointment);
 
-    app.listen(80, '0.0.0.0');
+  app.listen(80, "0.0.0.0");
 
-    console.info("Server listening...");
+  console.info("Server listening...");
 }
