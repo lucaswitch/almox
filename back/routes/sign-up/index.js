1444befe1aa48e53f21e1ceb83578f8073ca7f1d
Amanda <amanda.fo@sempreceub.com>
2023-06-23 00:03:38 -0300

    remove changes

--- back/routes/sign-up/index.js
+++ back/routes/sign-up/index.js
@@ -1,31 +1,19 @@
 import { User } from "../../models/user.js";
-import { validate } from "../../yup.js";
-import { object, string } from "yup";
 import moment from "moment";
 
 
 export async function createUser(request, response) {
+  const { username, password, full_name } =
+      request.body;
 
-  const signUpSchema = object({
-    username: string().required(),
-    password: string().required(),
-    full_name: string().required(),
+
+  const newUser = await User.create({
+    username: username,
+    password: password,
+    full_name: full_name,
+    created_at: moment().utc().unix(),
+    updated_at: moment().utc().unix(),
   });
 
-  const validation = await validate(request.body, signUpSchema);
-
-  if (!validation.valid) {
-    console.log("Error validation")
-    return response.status(400).json({ errors: validation.errors });
-  }
-
-  return response.status(200).json({
-    newUser : await User.create({
-      username: username,
-      password: password,
-      full_name: full_name,
-      created_at: moment().utc().unix(),
-      updated_at: moment().utc().unix(),
-    })
-  });
+  return response.status(200).json(newUser);
 }
