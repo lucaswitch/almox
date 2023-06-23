db0b799570b2ff3ae744b2eedfafb6dfb8e56794
Amanda <amanda.fo@sempreceub.com>
2023-06-22 23:44:26 -0300

    fix

--- back/server.js
+++ back/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { signIn } from "./routes/sign-in/index.js";
-import { createUser } from "./routes/sign-Up/index.js";
+import { createUser } from "./routes/sign-up/index.js";
 import bodyParser from "body-parser";
 import { listLabs } from "./routes/labs/index.js";
 import { listMaterials } from "./routes/materials/index.js";
