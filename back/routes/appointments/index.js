b54d52a9a70815ca69b4df31991d977a4945fc91
Amanda <amanda.fo@sempreceub.com>
2023-06-23 00:27:47 -0300

    fix appointments

--- back/routes/appointments/index.js
+++ back/routes/appointments/index.js
@@ -23,38 +23,43 @@ export async function createAppointment(request, response) {
   const { lab_id, student_capacity, scheduled_at, material_ids, note } =
     request.body;
 
-  const lab = await Lab.findByPk(lab_id);
-  const materials = [];
-  for (const { id, quantity } of material_ids) {
-    materials.push(await Material.findByPk(id));
-  }
-
-  // De fato criar a marcação.
-  const appointment = await Appointment.create({
-    lab_id: lab.id,
-    scheduled_at: scheduled_at,
-    student_capacity: student_capacity,
-    note: note,
-    created_at: moment().utc().unix(),
-    updated_at: moment().utc().unix(),
-    scheduled_by: 1,
-  });
-
-  for (const i in materials) {
-    const material = materials[i];
-    await AppointmentMaterial.create({
-      material_id: material.id,
-      appointment_id: appointment.id,
-      quantity: material_ids[i].quantity,
+  try {
+    const lab = await Lab.findByPk(lab_id);
+    const materials = [];
+    for (const { id, quantity } of material_ids) {
+      materials.push(await Material.findByPk(id));
+    }
+  
+    // De fato criar a marcação.
+    const appointment = await Appointment.create({
+      lab_id: lab.id,
+      scheduled_at: scheduled_at,
+      student_capacity: student_capacity,
+      note: note,
       created_at: moment().utc().unix(),
       updated_at: moment().utc().unix(),
+      scheduled_by: 1,
     });
+  
+    for (const i in materials) {
+      const material = materials[i];
+      await AppointmentMaterial.create({
+        material_id: material.id,
+        appointment_id: appointment.id,
+        quantity: material_ids[i].quantity,
+        created_at: moment().utc().unix(),
+        updated_at: moment().utc().unix(),
+      });
+    }
+  
+    return response.status(200).json({
+      appointment,
+      materials: materials.map(function (material, i) {
+        return { ...material.dataValues, quantity: material_ids[i].quantity };
+      }),
+    });
+  } catch(err) {
+    return response.status(400).json(err.toString());
   }
-
-  return response.status(200).json({
-    appointment,
-    materials: materials.map(function (material, i) {
-      return { ...material.dataValues, quantity: material_ids[i].quantity };
-    }),
-  });
+ 
 }
